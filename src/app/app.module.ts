67c7a2f72bafb7ec1d0fa185e521ca2bf9761816
stLmpp <gui.stlmpp@hotmail.com>
2021-07-21 14:08:08 -0300

    feat(): added title feat(header): added header toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
